2d5541e8f205c6624e9103bb3eaf9481b067598d
ZoeRoy360 <zoeroy@seattleacademy.org>
2026-03-13 21:09:57 +0000

    sync changes

---
+++ jokes.py
@@ -0,0 +1,2 @@
+name=input("What's your name?")
+print(f"Want to hear a joke {name}?")
---
+++ project1.py
@@ -0,0 +1,12 @@
+name=input("Hi! What is your name? ")
+if("name>Zoe"):
+    print("No way, we have the same name!")
+else:
+print(f"Let's play madlibs {name}!")
+input("")
+place1=input("Give me a place: ")
+input("")
+character1=input("Now give me a name: ")
+input("")
+animal1=input("Give me an animal: ")
+input("")
